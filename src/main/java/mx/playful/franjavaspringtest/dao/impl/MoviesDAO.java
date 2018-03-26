8429e3f1ed0e4fcd64fa2756fc732e05e65ad022
eiseidf <eiseidf@10.175.111.105>
2018-03-26 15:23:16 -0600

    Little change to test

--- src/main/java/mx/playful/franjavaspringtest/dao/impl/MoviesDAO.java
+++ src/main/java/mx/playful/franjavaspringtest/dao/impl/MoviesDAO.java
@@ -11,7 +11,7 @@ public class MoviesDAO implements IMoviesDAO {
 	@Override
 	public MoviesDTO getTitle(Integer id) throws Exception {
 		// Database access
-		return new MoviesDTO(id, " A Clockwork Orange - 1970");
+		return new MoviesDTO(id, " A Clockwork Orange - 1970 [15:22]");
 	}
 
 	@Override
